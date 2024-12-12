2cedddd0ef7e5844600e958653f8341911fb630c
dinoskilol <dinoyordanov@gmail.com>
2024-12-12 15:56:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+if (!document.body.classList.contains('dark')) {
+  document.body.classList.add('dark');
+}
+
+function toggle() {
+    document.body.classList.add('animation-ready');
+    document.body.classList.toggle('dark');
+  }
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 32) {
+      toggle();
+    }
+  });
+  
+  document.addEventListener('click', function() {
+    toggle();
+  });
